fe2a0b4cda8c951918cf146c0c59850687a287df
Jeremythong2002 <jeremythong2002@gmail.com>
2024-10-17 15:28:13 +0800

    isAvailableForReservationMethod still needs to be updated to check the one tier up rooms

---
+++ HolidayReservationSystem-ejb/src/java/ejb/sessions/stateless/HotelInventory.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package ejb.sessions.stateless;
+
+import entity.Room;
+import entity.RoomType;
+import java.util.Date;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author jeremy
+ */
+@Stateless
+public class HotelInventory implements HotelInventoryRemote, HotelInventoryLocal {
+
+    @PersistenceContext(unitName = "HolidayReservationSystem-ejbPU")
+    private EntityManager em;
+
+    public HotelInventory() {
+    }
+
+    //dynamically generate the inventory of the hotel to prevent sync errors
+    public boolean availableRoomsForReservation(Date startDate, Date endDate, RoomType roomType) {
+        Query roomQuery = em.createQuery("SELECT r FROM Room r WHERE r.roomType =:roomType");
+        roomQuery.setParameter("roomType", roomType);
+        
+        //handles the case where there are no rooms of that room type in the first place
+        if (roomQuery.getResultList().isEmpty()) {
+            return false;
+        }
+     
+        List<Room> rooms = roomQuery.getResultList();
+        
+        Query reservationQuery = em.createQuery("SELECT r FROM Reservation r WHERE r.roomType =:roomType"); 
+        reservationQuery.setParameter("roomType", roomType); 
+        List<Reservation> reservations = reservationQuery.getResultList();
+        
+        int count = 0; 
+        for (Reservation r : reservations) {
+            if ((r.getStartDate()>= startDate && r.getStartDate() <= endDate) || (r.getEndDate() <= endDate && r.getEndDate() >= startDate)) {
+                count += 1;
+            }
+        }
+        
+        //if more rooms than booking then can make reservation
+        return rooms.size() > count;
+        
+    }
+
+
+}
